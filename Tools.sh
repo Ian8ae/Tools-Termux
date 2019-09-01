54720fa963fd5ab671ef1ac36777f03568f36404
Ian8ae <54733469+Ian8ae@users.noreply.github.com>
2019-09-01 02:16:34 +0000

    Create Tools.sh

---
+++ Tools.sh
@@ -0,0 +1,345 @@
+cy='\033[36;1m'
+i='\033[32;1m'
+me='\033[31;1m'
+bi='\033[34;1m'
+pu='\033[37;1m'
+echo $me"• "$cy"—"$me" • ============================================= "$me"• "$cy"—"$me" •"
+echo $cy"| "$me"•"$cy" |"$me"           ×××"$bi"LIST TOOLS TERMUX"$me"×××             "$cy"|"$me" • "$cy"|"
+echo $me"• "$cy"—"$me" • ============================================= "$me"• "$cy"—"$me" •"
+echo $cy"| "$me"•"$cy" |"$me"Author"$cy" : "$i"Ian Bae                               "$cy"|"$me" • "$cy"|"
+echo $cy"| "$me"•"$cy" |"$me"Facebook"$cy" : "$i"Ian Bae                             "$cy"|"$me" • "$cy"|"
+echo $cy"| "$me"•"$cy" |"$me"Thanks For"$cy" :"$i"[MR.P3RC1L] [MR.S∆MAx] [MR.B]      "$cy"|"$me" • "$cy"|"
+echo $cy"| "$me"•"$cy" |            "$i"[MR.BUM1] [MR.XXZ] [cyber] [%MR.S%]"$cy"|"$me" • "$cy"|"
+echo $cy"| "$me"•"$cy" |            "$i"[AldykaPrygh] [F1SH] [4RD1]        "$cy"|"$me" • "$cy"|"
+echo $cy"| "$me"•"$cy" |            "$i"And [M•C•A_MEMBER]                 "$cy"|"$me" • "$cy"|"
+echo $me"• "$cy"—"$me" • ============================================= "$me"• "$cy"—"$me" •"
+echo $i" ["$bi"*"$i"]"$me"—————————————————————————————————————————————————"$i"["$bi"*"$i"]"
+echo $me"  | "$pu"[EXIT]"$i"EXIT PROGRAM/KELUAR PROGRAM                 "$me"|"
+echo $me"  | "$pu"[0]"$i"Install Tools (MUST INSTALLED)                 "$me"|"
+echo $me"  | "$pu"[00]"$i"Install Ingredients (MUST INSTALLED)±150MB    "$me"|"
+echo $i" ["$bi"*"$i"]"$me"—————————————————————————————————————————————————"$i"["$bi"*"$i"]"
+echo $me"  | "$pu"[1]"$i"Bengkel Facebook                   "$me"   ^        "$me"|"
+echo $me"  | "$pu"[2]"$i"Katoolin                           "$me"  / \       "$me"|"
+echo $me"  | "$pu"[3]"$i"Browser                            "$me" /   \      "$me"|"
+echo $me"  | "$pu"[4]"$i"A-Rat                              "$me"/     \     "$me"|"
+echo $me"  | "$pu"[5]"$i"LiteDDOS                           "$me"<     >     "$me"|"
+echo $me"  | "$pu"[6]"$i"Dark-FB                            "$cy"<     >     "$me"|"
+echo $me"  | "$pu"[7]"$i"BruteForce FB PHP                  "$cy"<     >     "$me"|"
+echo $me"  | "$pu"[8]"$i"BruteForce FB Cracker V.3          "$cy"<     >     "$me"|"
+echo $me"  | "$pu"[9]"$i"Wifi-Hacker [ROOT]                 "$cy"<     >     "$me"|"
+echo $me"  | "$pu"[10]"$i"BruteForce FB MBF                 "$cy"<     >     "$me"|"
+echo $me"  | "$pu"[11]"$i"ButeForce FB [ROOT]               "$cy"<     >     "$me"|"
+echo $me"  | "$pu"[12]"$i"Bugging Camera/Sadap Kamera       "$cy"<     >     "$me"|"
+echo $me"  | "$pu"[13]"$i"IP GeoLocation                    "$cy"<     >     "$me"|"
+echo $me"  | "$pu"[14]"$i"IP Locator                        "$cy"<     >     "$me"|"
+echo $me"  | "$pu"[15]"$i"CNK Spam Tools                  "$bi"("$me"========="$bi")""   "$me"|"
+echo $me"  | "$pu"[16]"$i"Hammer DDOS                        "$cy"[   ]      "$me"|"
+echo $me"  | "$pu"[17]"$i"Yahoo EmailVuln                    "$cy"[   ]      "$me"|"
+echo $me"  | "$pu"[18]"$i"Pofile Guard FB                    "$cy"[   ]      "$me"|"
+echo $me"  | "$pu"[19]"$i"Report FB                          "$cy"[   ]      "$me"|"
+echo $me"  | "$pu"[20]"$i"Information FB                   "$me"["$bi"======="$me"]    "$me"|"
+echo $i" ["$bi"*"$i"]"$me"—————————————————————————————————————————————————"$i"["$bi"*"$i"]"$cy
+read -p"  Silahkan Pilih Tools Nomor=" pil
+
+if [ $pil = exit ]
+  then
+   clear
+    figlet -f slant "EXITING"|lolcat
+     figlet -f slant "PROGRAM"|lolcat
+      sleep 1
+       clear
+        sleep 0.5
+         figlet "SUCCESS"|lolcat
+          sleep 0.7
+           clear
+            exit
+             fi
+
+if [ $pil = EXIT ]
+  then
+   clear
+    figlet -f slant "EXITING"|lolcat
+     figlet -f slant "PROGRAM"|lolcat
+      sleep 2
+       clear
+        sleep 0.5
+         figlet "SUCCESS"|lolcat
+          sleep 0.7
+           clear
+            exit
+             fi
+
+if [ $pil = 00 ]
+then
+clear
+apt update && apt upgrade
+pkg update && pkg upgrade
+pkg install w3m
+pkg install -y
+pkg install gnupg
+pkf install openssl-tool
+pkg install ssh
+pkg install openssh
+pkg install curl
+pkg install tsu
+pkg install tsu-y
+pkg install python
+pkg install python2
+pkg install python3
+pkg install bash
+pkg install toilet
+pkg install lolcat
+pip2 install urllib3 chardet certifi idna requests
+pip2 install requests
+pkg install openssh
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install figlet
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+pkg install ruby
+sleep 1
+clear
+figlet -f slant "SUCCESS"|lolcat
+sleep 2
+clear
+sh Tools.sh
+fi
+
+if [ $pil = 0 ]
+then
+clear
+git clone https://github.com/hatakecnk/CNK-SPAM
+git clone https://github.com/4L13199/LITEDDOS
+git clone https://github.com/thelinuxchoice/locator
+git clone https://github.com/maldevel/IPGeolocation
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+git clone https://github.com/Xi4u7/A-Rat
+git clone https://github.com/FR13ND8/fbbrute
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3
+git clone https://github.com/FR13ND8/mbf
+git clone https://github.com/thelinuxchoice/facebash.git
+git clone https://github.com/FR13ND8/EmailVuln
+git clone https://github.com/FR13ND8/ProfileGuardFb
+git clone https://github.com/IlayTamvan/Report
+git clone https://github.com/AMVengeance/FB-React
+git clone https://github.com/FR13ND8/InfoFB
+git clone https://github.com/FR13ND8/autolike
+git clone https://github.com/thelinuxchoice/shellphish.git
+git clone https://github.com/thelinuxchoice/blackeye.git
+git clone https://github.com/UndeadSec/SocialFish.git
+git clone https://github.com/evait-security/weeman.git
+git clone https://github.com/esc0rtd3w/wifi-hacker
+git clone http://github.com/cyweb/hammer
+git clone https://github.com/thelinuxchoice/saycheese
+git clone https://github.com/rezadkim/dark-fb
+sleep 1
+clear
+figlet -f slant "SUCCES"|lolcat
+sleep 2
+clear
+sh Tools.sh
+fi
+
+
+if [ $pil = 1 ]
+  then
+   clear
+    figlet "WAIT"|lolcat
+     sleep 2
+       cd BRUTEFORCEnew
+        sh new.sh
+         fi
+
+if [ $pil = 2 ]
+   then
+    clear
+     figlet "WAIT"|lolcat
+       sleep 2
+        cd katoolin
+         python2 katoolin.py
+          fi
+
+if [ $pil = 3 ]
+  then
+   clear
+     figlet "WAIT"|lolcat
+     sleep 2
+     w3m www.google.com
+        fi
+
+if [ $pil = 4 ]
+  then
+   clear
+     figlet "WAIT"|lolcat
+      sleep 2
+       cd A-Rat
+        python2 A-Rat.py
+         fi
+
+if [ $pil = 5 ]
+  then
+   clear
+    figlet "WAIT"|lolcat
+    sleep 2
+cd LITEDDOS
+python2 LITEDDOS.py
+exit
+         fi
+if [ $pil = 6 ]
+   then
+    clear
+     figlet "WAIT"|lolcat
+     sleep 2
+      cd dark-fb
+       python2 dark.py
+        fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet "WAIT"|lolcat
+sleep 2
+cd fbbrute
+php fb.php
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet "WAIT"|lolcat
+sleep 2
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet "WAIT"|lolcat
+sleep 2
+cd wifi-hacker
+chmod +x wifi-hacker.sh
+./wifi-hacker.sh
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figleyt "WAIT"|lolcat
+sleep 2
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet "WAIT"|lolcat
+sleep 2
+cd facebash
+bash facebash.sh
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet "WAIT"|lolcat
+sleep 2
+cd saycheese
+bash saycheese.sh
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet "WAIT"|lolcat
+sleep 2
+cd IPGeolocation
+pip2 install -r requirements.txt
+python3 ipgeolocation.py
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet "WAIT"|lolcat
+cd locator
+tsu
+bash locator.sh
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet "WAIT"|lolcat
+sleep 2
+cd CNK-SPAM
+chmod +x cnk-spam.py
+python2 cnk-spam.py
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet "WAIT"|lolcat
+sleep2
+cd hammer
+python3 hammer.py
+exit
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet "WAIT"|lolcat
+sleep 2
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $pil = 18 ]
+then
+clear
+figlet "WAIT"|lolcat
+sleep 2
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $pil = 19 ]
+then
+clear
+figlet "WAIT"|lolcat
+sleep 2
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 20 ]
+then
+clear
+figlet "WAIT"|lolcat
+sleep 2
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+else:
+clear
+figlet -f slant "SORRY"|lolcat
+figlet -f slant "YOUR"|lolcat
+figlet -f slant "INPUT"|lolcat
+figlet -f slant "UNAVAIBLE"|lolcat
+sleep 0.3
+clear
+sh Tools.sh
+
